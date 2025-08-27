37444268bfcda7c87c077a42ab2f85955752cec8
alecxander567 <alecxanderespaldon21@gmail.com>
2025-08-27 17:08:04 +0800

    Initial Commit

---
+++ Expense Tracker App/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace Expense_Tracker_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblWelcomeMessage = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.txtBoxInput = new System.Windows.Forms.TextBox();
+            this.btnAddExpense = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.comboCategory = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTitle.Location = new System.Drawing.Point(55, 54);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(561, 55);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Expense Tracker App📃";
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(723, 65);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(0, 29);
+            this.lblDate.TabIndex = 1;
+            // 
+            // lblWelcomeMessage
+            // 
+            this.lblWelcomeMessage.AutoSize = true;
+            this.lblWelcomeMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcomeMessage.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblWelcomeMessage.Location = new System.Drawing.Point(68, 137);
+            this.lblWelcomeMessage.Name = "lblWelcomeMessage";
+            this.lblWelcomeMessage.Size = new System.Drawing.Size(202, 32);
+            this.lblWelcomeMessage.TabIndex = 3;
+            this.lblWelcomeMessage.Text = "Good Morning!";
+            this.lblWelcomeMessage.Click += new System.EventHandler(this.lblWelcomeMessage_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ActiveCaption;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(65, 197);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1080, 410);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(867, 54);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(267, 26);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // txtBoxInput
+            // 
+            this.txtBoxInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInput.Location = new System.Drawing.Point(256, 692);
+            this.txtBoxInput.Name = "txtBoxInput";
+            this.txtBoxInput.Size = new System.Drawing.Size(624, 44);
+            this.txtBoxInput.TabIndex = 6;
+            this.txtBoxInput.TextChanged += new System.EventHandler(this.txtBoxInput_TextChanged);
+            // 
+            // btnAddExpense
+            // 
+            this.btnAddExpense.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnAddExpense.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAddExpense.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddExpense.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnAddExpense.Location = new System.Drawing.Point(983, 849);
+            this.btnAddExpense.Name = "btnAddExpense";
+            this.btnAddExpense.Size = new System.Drawing.Size(205, 78);
+            this.btnAddExpense.TabIndex = 7;
+            this.btnAddExpense.Text = "Add Expense";
+            this.btnAddExpense.UseVisualStyleBackColor = false;
+            this.btnAddExpense.Click += new System.EventHandler(this.btnAddExpense_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(33, 632);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Add New Expense : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(51, 701);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 29);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Description : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(51, 769);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 29);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Amount : ";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.Location = new System.Drawing.Point(256, 754);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(624, 44);
+            this.txtAmount.TabIndex = 12;
+            this.txtAmount.TextChanged += new System.EventHandler(this.txtAmount_TextChanged);
+            // 
+            // comboCategory
+            // 
+            this.comboCategory.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboCategory.FormattingEnabled = true;
+            this.comboCategory.Items.AddRange(new object[] {
+            "Food",
+            "Transportation",
+            "Materials",
+            "Tuition",
+            "Extras"});
+            this.comboCategory.Location = new System.Drawing.Point(256, 831);
+            this.comboCategory.Name = "comboCategory";
+            this.comboCategory.Size = new System.Drawing.Size(211, 37);
+            this.comboCategory.TabIndex = 13;
+            this.comboCategory.Text = "Select Category";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(51, 830);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 29);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Category : ";
+            // 
+            // btnExport
+            // 
+            this.btnExport.BackColor = System.Drawing.Color.Teal;
+            this.btnExport.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExport.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExport.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnExport.Location = new System.Drawing.Point(758, 849);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(205, 78);
+            this.btnExport.TabIndex = 15;
+            this.btnExport.Text = "Export CSV";
+            this.btnExport.UseVisualStyleBackColor = false;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(908, 632);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(86, 29);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Total : ";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotal.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblTotal.Location = new System.Drawing.Point(978, 632);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(0, 29);
+            this.lblTotal.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Navy;
+            this.ClientSize = new System.Drawing.Size(1221, 931);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboCategory);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAddExpense);
+            this.Controls.Add(this.txtBoxInput);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.lblWelcomeMessage);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.lblTitle);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(30);
+            this.Text = "Expense Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblWelcomeMessage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox txtBoxInput;
+        private System.Windows.Forms.Button btnAddExpense;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBoxAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.ComboBox comboCategory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotal;
+    }
+}
+
---
+++ Expense Tracker App/Form1.cs
@@ -0,0 +1,277 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+using JsonFormatting = Newtonsoft.Json.Formatting;
+
+namespace Expense_Tracker_App
+{
+    public partial class Form1 : Form
+    {
+        private List<Expense> expenses = new List<Expense>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string GetGreetingMessage()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 5 && hour < 12)
+            {
+                return "Good Morning!☀️";
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                return "Good Afternoon!⛅";
+            }
+            else
+            {
+                return "Good Evening!🌙";
+            }
+        }
+
+        private void SaveExpensesToLocal()
+        {
+            string json = JsonConvert.SerializeObject(expenses, JsonFormatting.Indented);
+            File.WriteAllText("expenses.json", json);
+        }
+
+        private void LoadExpensesFromLocal()
+        {
+            if (File.Exists("expenses.json"))
+            {
+                string json = File.ReadAllText("expenses.json");
+                expenses = JsonConvert.DeserializeObject<List<Expense>>(json) ?? new List<Expense>();
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = expenses;
+            }
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            string fileName = $"Expenses_{DateTime.Now:yyyy_MM}.csv";
+
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                writer.WriteLine("Description,Category,Amount,Date");
+                foreach (var exp in expenses)
+                {
+                    writer.WriteLine($"{exp.Description},{exp.Category},{exp.Amount},{exp.Date:yyyy-MM-dd}");
+                }
+            }
+
+            MessageBox.Show("Expenses exported to " + fileName);
+
+            expenses.Clear();
+            dataGridView1.DataSource = null;
+            File.Delete("expenses.json"); 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblWelcomeMessage.Text = GetGreetingMessage();
+
+            dataGridView1.Columns.Clear();
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Description",
+                DataPropertyName = "Description"
+            });
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Category",
+                DataPropertyName = "Category"
+            });
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Amount",
+                DataPropertyName = "Amount"
+            });
+
+            dataGridView1.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                HeaderText = "Date",
+                DataPropertyName = "Date"
+            });
+
+            var editButton = new DataGridViewButtonColumn
+            {
+                HeaderText = "Edit",
+                Text = "✍🏾",
+                UseColumnTextForButtonValue = true,
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.None, 
+                Width = 60 
+            };
+            dataGridView1.Columns.Add(editButton);
+
+            var deleteButton = new DataGridViewButtonColumn
+            {
+                HeaderText = "Delete",
+                Text = "🚮",
+                UseColumnTextForButtonValue = true,
+                AutoSizeMode = DataGridViewAutoSizeColumnMode.None,
+                Width = 60
+            };
+            dataGridView1.Columns.Add(deleteButton);
+
+            LoadExpensesFromLocal();
+            UpdateTotal();
+        }
+
+        private void btnAddExpense_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtBoxInput.Text) ||
+                string.IsNullOrWhiteSpace(txtAmount.Text) ||
+                comboCategory.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please fill in all fields.");
+                return;
+            }
+
+            Expense expense = new Expense
+            {
+                Description = txtBoxInput.Text,
+                Category = comboCategory.Text,   
+                Amount = decimal.Parse(txtAmount.Text),
+                Date = dateTimePicker1.Value
+            };
+
+            expenses.Add(expense);
+
+            dataGridView1.DataSource = null;
+            dataGridView1.DataSource = expenses;
+
+            UpdateTotal();
+
+            SaveExpensesToLocal();
+
+            txtBoxInput.Clear();
+            txtAmount.Clear();
+            comboCategory.SelectedIndex = -1;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return; 
+
+            var expense = expenses[e.RowIndex];
+
+            if (dataGridView1.Columns[e.ColumnIndex].HeaderText == "Edit")
+            {
+                string newDescription = Prompt.ShowDialog("Edit Description:", "Edit", expense.Description);
+                string newCategory = Prompt.ShowDialog("Edit Category:", "Edit", expense.Category);
+                string newAmount = Prompt.ShowDialog("Edit Amount:", "Edit", expense.Amount.ToString());
+
+                if (decimal.TryParse(newAmount, out decimal updatedAmount))
+                {
+                    expense.Description = newDescription;
+                    expense.Category = newCategory;
+                    expense.Amount = updatedAmount;
+
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = expenses;
+
+                    UpdateTotal();
+
+                    SaveExpensesToLocal();
+                }
+            }
+
+            if (dataGridView1.Columns[e.ColumnIndex].HeaderText == "Delete")
+            {
+                var confirm = MessageBox.Show("Are you sure you want to delete this expense?",
+                                              "Confirm Delete",
+                                              MessageBoxButtons.YesNo);
+                if (confirm == DialogResult.Yes)
+                {
+                    expenses.RemoveAt(e.RowIndex);
+                    dataGridView1.DataSource = null;
+                    dataGridView1.DataSource = expenses;
+
+                    UpdateTotal();
+
+                    SaveExpensesToLocal();
+                }
+            }
+        }
+
+        private void btnExport_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JSON files (*.json)|*.json|All files (*.*)|*.*";
+            saveFileDialog.Title = "Export Expenses";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string json = JsonConvert.SerializeObject(expenses, Newtonsoft.Json.Formatting.Indented);
+                File.WriteAllText(saveFileDialog.FileName, json);
+                MessageBox.Show("Expenses exported successfully!", "Export", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void UpdateTotal()
+        {
+            decimal total = 0;
+            foreach (var exp in expenses)
+            {
+                total += exp.Amount;
+            }
+            lblTotal.Text = "₱" + total.ToString("N2");
+        }
+
+        private void txtBoxInput_TextChanged(object sender, EventArgs e) { }
+
+        private void txtAmount_TextChanged(object sender, EventArgs e) { }
+
+        private void lblWelcomeMessage_Click(object sender, EventArgs e){ }
+    }
+
+    public class Expense
+    {
+        public string Description { get; set; }
+        public string Category { get; set; } 
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption, string defaultValue = "")
+        {
+            Form prompt = new Form()
+            {
+                Width = 400,
+                Height = 180,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+            Label textLabel = new Label() { Left = 20, Top = 20, Text = text, Width = 340 };
+            TextBox textBox = new TextBox() { Left = 20, Top = 50, Width = 340, Text = defaultValue };
+            Button confirmation = new Button() { Text = "Ok", Left = 200, Width = 80, Top = 90, DialogResult = DialogResult.OK };
+            Button cancel = new Button() { Text = "Cancel", Left = 280, Width = 80, Top = 90, DialogResult = DialogResult.Cancel };
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            cancel.Click += (sender, e) => { textBox.Text = defaultValue; prompt.Close(); };
+
+            prompt.Controls.Add(textBox);
+            prompt.Controls.Add(confirmation);
+            prompt.Controls.Add(cancel);
+            prompt.Controls.Add(textLabel);
+            prompt.AcceptButton = confirmation;
+            prompt.CancelButton = cancel;
+
+            return prompt.ShowDialog() == DialogResult.OK ? textBox.Text : defaultValue;
+        }
+    }
+}
